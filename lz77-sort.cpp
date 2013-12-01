8223737c05c7fca996d4f21962a3a29cb459150f
Matt Jenny <mjenny@Matts-MacBook-Pro-2.local>
2013-12-01 01:28:57 -0500

    CA-LZ77 sort implemented; output in non-LZ77 form.

--- lz77-sort.cpp
+++ lz77-sort.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <list>
 #include <CircBuffer.h>
 
 using namespace std;
@@ -19,11 +22,23 @@ char delim[2] = {TERMINAL_DELIMITER, NONTERMINAL_DELIMITER};
 bool verbose = false;
 CircBuffer dict(WINDOW_LENGTH);
 std::vector<string> data;
+list<string> lit;
+unordered_map<string, unsigned int> counts;
+
+bool compare_ignore_case(string first, string second) {
+	unsigned int i=0;
+	while (i<first.length() && i<second.length()) {
+		if (tolower(first[i]) < tolower(second[i])) return true;
+		if (tolower(first[i]) > tolower(second[i])) return false;
+		i++;
+	}
+	return (first.length() < second.length());
+}
 
 void process_terminal(string terminal) {
 	if (verbose) printf("(0,%s):  %s\n", terminal.c_str(), terminal.c_str());
-	data.push_back(terminal);
 	dict.put(terminal);
+	counts.at(terminal)++;
 }
 
 void process_nonterminal(int offset, char length) {
@@ -34,20 +49,48 @@ void process_nonterminal(int offset, char length) {
 	dict.get(index, decoded_str, length); //get the data encoded by the pointer
 	dict.put(decoded_str, length); //store this data in the dictionary buffer
 	for (i=0; i<length; i++) {
-		data.push_back(decoded_str[i]); 
+		counts.at(decoded_str[i])++; 
 		if (verbose) printf("%s-", decoded_str[i].c_str());
 	}
 	if (verbose) printf("\n");
 	delete [] decoded_str;
 }
 
-void decompress() {
+void lz77_sort() {
+	unordered_set<string> lit_set;
 	string line;
 	char current_delimiter;
 	istreambuf_iterator<char> eos;
 	line = string(istreambuf_iterator<char> (cin), eos);
+
+	//Find all unique literals
 	current_delimiter = line.at(0);
 	size_t prev = 1, pos;
+	while(prev < line.length()) {
+		if (current_delimiter == TERMINAL_DELIMITER) {
+			pos = line.find_first_of(delim, prev);
+			if (pos == string::npos) {
+				pos = line.length()-1;
+				lit_set.insert(line.substr(prev, line.length()-prev));
+			}
+			else
+				lit_set.insert(line.substr(prev, pos-prev));
+		} else {
+			pos = prev+2;
+		}
+		current_delimiter = line.at(pos);
+		prev = pos+1;
+	}
+
+	for (unordered_set<string>::iterator it = lit_set.begin(); it != lit_set.end(); ++it) {
+		lit.push_back(*it);
+		counts.insert(make_pair<string, unsigned int>(*it, 0));
+	}
+	lit.sort(compare_ignore_case);
+
+	//Tally the numbers of each literal
+	current_delimiter = line.at(0);
+	prev = 1, pos = 0;
 	while(prev < line.length()) {
 		if (current_delimiter == TERMINAL_DELIMITER) {
 			pos = line.find_first_of(delim, prev);
@@ -67,9 +110,9 @@ void decompress() {
 		prev = pos+1;
 	}
 
-	int i;
-	for (i=0; i<data.size(); i++) {
-		printf("%s ", data.at(i).c_str());
+	printf("\nRESULTS:\n");
+	for(list<string>::iterator lit_it = lit.begin(); lit_it != lit.end(); ++lit_it) {
+		printf("(%s,%u) ",(*lit_it).c_str(),counts.at(*lit_it));
 	}
 	printf("\n");
 }
@@ -78,6 +121,6 @@ int main(int argc, char** argv) {
 	if (argc > 1 && strcmp("-v", argv[1]) == 0) {
 		verbose = true;
 	}
-	decompress();
+	lz77_sort();
 	return 0;
 }
